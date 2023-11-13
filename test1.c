7d850da63bfe41695985a1fa03db8a6a003433a3
SpilopeliaMisac <126560178+SpilopeliaMisac@users.noreply.github.com>
2023-11-13 18:43:56 +0800

    Rename cpu0_main.c to test1.c

--- test1.c
+++ test1.c
@@ -1,46 +1,46 @@
 /*********************************************************************************************************************
-* TC264 Opensourec Library ����TC264 ��Դ�⣩��һ�����ڹٷ� SDK �ӿڵĵ�������Դ��
-* Copyright (c) 2022 SEEKFREE ��ɿƼ�
+* TC264 Opensourec Library 即（TC264 开源库）是一个基于官方 SDK 接口的第三方开源库
+* Copyright (c) 2022 SEEKFREE 逐飞科技
 *
-* ���ļ��� TC264 ��Դ���һ����
+* 本文件是 TC264 开源库的一部分
 *
-* TC264 ��Դ�� ���������
-* �����Ը���������������ᷢ���� GPL��GNU General Public License���� GNUͨ�ù�������֤��������
-* �� GPL �ĵ�3�棨�� GPL3.0������ѡ��ģ��κκ����İ汾�����·�����/���޸���
+* TC264 开源库 是免费软件
+* 您可以根据自由软件基金会发布的 GPL（GNU General Public License，即 GNU通用公共许可证）的条款
+* 即 GPL 的第3版（即 GPL3.0）或（您选择的）任何后来的版本，重新发布和/或修改它
 *
-* ����Դ��ķ�����ϣ�����ܷ������ã�����δ�������κεı�֤
-* ����û�������������Ի��ʺ��ض���;�ı�֤
-* ����ϸ����μ� GPL
+* 本开源库的发布是希望它能发挥作用，但并未对其作任何的保证
+* 甚至没有隐含的适销性或适合特定用途的保证
+* 更多细节请参见 GPL
 *
-* ��Ӧ�����յ�����Դ���ͬʱ�յ�һ�� GPL �ĸ���
-* ���û�У������<https://www.gnu.org/licenses/>
+* 您应该在收到本开源库的同时收到一份 GPL 的副本
+* 如果没有，请参阅<https://www.gnu.org/licenses/>
 *
-* ����ע����
-* ����Դ��ʹ�� GPL3.0 ��Դ����֤Э�� ������������Ϊ���İ汾
-* ��������Ӣ�İ��� libraries/doc �ļ����µ� GPL3_permission_statement.txt �ļ���
-* ����֤������ libraries �ļ����� �����ļ����µ� LICENSE �ļ�
-* ��ӭ��λʹ�ò����������� ���޸�����ʱ���뱣����ɿƼ��İ�Ȩ����������������
+* 额外注明：
+* 本开源库使用 GPL3.0 开源许可证协议 以上许可申明为译文版本
+* 许可申明英文版在 libraries/doc 文件夹下的 GPL3_permission_statement.txt 文件中
+* 许可证副本在 libraries 文件夹下 即该文件夹下的 LICENSE 文件
+* 欢迎各位使用并传播本程序 但修改内容时必须保留逐飞科技的版权声明（即本声明）
 *
-* �ļ�����          cpu0_main
-* ��˾����          �ɶ���ɿƼ����޹�˾
-* �汾��Ϣ          �鿴 libraries/doc �ļ����� version �ļ� �汾˵��
-* ��������          ADS v1.9.4
-* ����ƽ̨          TC264D
-* ��������          https://seekfree.taobao.com/
+* 文件名称          cpu0_main
+* 公司名称          成都逐飞科技有限公司
+* 版本信息          查看 libraries/doc 文件夹内 version 文件 版本说明
+* 开发环境          ADS v1.9.4
+* 适用平台          TC264D
+* 店铺链接          https://seekfree.taobao.com/
 *
-* �޸ļ�¼
-* ����              ����                ��ע
+* 修改记录
+* 日期              作者                备注
 * 2022-09-15       pudding            first version
 ********************************************************************************************************************/
 #include "zf_common_headfile.h"
 #pragma section all "cpu0_dsram"
-// ���������#pragma section all restore���֮���ȫ�ֱ���������CPU0��RAM��
+// 将本语句与#pragma section all restore语句之间的全局变量都放在CPU0的RAM中
 
-// �������ǿ�Դ��չ��� ��������ֲ���߲��Ը���������
-// �������ǿ�Դ��չ��� ��������ֲ���߲��Ը���������
-// �������ǿ�Դ��չ��� ��������ֲ���߲��Ը���������
+// 本例程是开源库空工程 可用作移植或者测试各类内外设
+// 本例程是开源库空工程 可用作移植或者测试各类内外设
+// 本例程是开源库空工程 可用作移植或者测试各类内外设
 
-// **************************** �������� ****************************
+// **************************** 代码区域 ****************************
 
 
 short angle_num=838,speed=1800;
@@ -48,7 +48,7 @@ const short middle_angle_num=838,speed_defa=1800;        //angle1:1050;800;610  
 
 struct Adc_struct
 {
-    unsigned short  LeftHori,RightHori,                 //���ֵ
+    unsigned short  LeftHori,RightHori,                 //电感值
                     LeftVert,RightVert,
 
                     HoriMax,HoriMin,
@@ -61,13 +61,13 @@ struct Adc_struct
                     LeftVert_Min,RightVert_Min;
 
     float           Err,Err_pre,
-                    LeftHori_value,RightHori_value,     //��ֵ����ֵ
+                    LeftHori_value,RightHori_value,     //二值化的值
                     LeftVert_value,RightVert_value;
 
     bool            temp1,temp2;
 }Adc;
 
-//��ȡ������ֵ����Сֵ
+//获取电感最大值和最小值
 void get_Adc_MaxMin()
 {
     Adc.LeftHori_Max = (Adc.LeftHori >= Adc.LeftHori_Max)?Adc.LeftHori:Adc.LeftHori_Max;
@@ -83,7 +83,7 @@ void get_Adc_MaxMin()
     Adc.RightVert_Min = (Adc.RightVert <= Adc.RightVert_Min)?Adc.RightVert:Adc.RightVert_Min;
 }
 
-//��ֵ�� �Լ� ���Ⱥ�
+//二值化 以及 求差比和
 void get_Adc_Err()
 {
     Adc.LeftHori_value =    1.0*(Adc.LeftHori - Adc.LeftHori_Min)/(Adc.LeftHori_Max - Adc.LeftHori_Min);
@@ -98,7 +98,7 @@ void get_Adc_Err()
     Adc.Err = (Adc.LeftHori_value - Adc.RightHori_value)/(Adc.LeftHori_value + Adc.RightHori_value);
 }
 
-unsigned short test1()   //ת��
+unsigned short test1()   //转弯
 {
     const unsigned short A=1200,B=200,C=200;
     static float temp1;
@@ -154,17 +154,17 @@ void ips_show()
 
 int core0_main(void)
 {
-    clock_init();                               // ��ȡʱ��Ƶ��<��ر���>
-    debug_init();                               // ��ʼ��Ĭ�ϵ��Դ���
+    clock_init();                               // 获取时钟频率<务必保留>
+    debug_init();                               // 初始化默认调试串口
 
 
-    pwm_init(ATOM0_CH1_P33_9, 50, 1000);        //�����ʼ��
-    pwm_init(ATOM0_CH6_P02_6, 17000, 1000);     //�����ʼ��
+    pwm_init(ATOM0_CH1_P33_9, 50, 1000);        //舵机初始化
+    pwm_init(ATOM0_CH6_P02_6, 17000, 1000);     //电机初始化
 
     //gpio_init(P02_6, GPO, 0, GPO_PUSH_PULL);
 
-    adc_init(ADC0_CH4_A4, ADC_8BIT);            //���г�ʼ��
-    adc_init(ADC0_CH7_A7, ADC_8BIT);            //��
+    adc_init(ADC0_CH4_A4, ADC_8BIT);            //左电感初始化
+    adc_init(ADC0_CH7_A7, ADC_8BIT);            //右
     adc_init(ADC0_CH5_A5, ADC_8BIT);
     adc_init(ADC0_CH6_A6, ADC_8BIT);
 
@@ -189,36 +189,36 @@ int core0_main(void)
     //gpio_init(P02_6, GPO, 1, GPO_PUSH_PULL);
 
 
-    cpu_wait_event_ready();         // �ȴ����к��ĳ�ʼ�����
+    cpu_wait_event_ready();         // 等待所有核心初始化完毕
     while (TRUE)
     {
-        // �˴���д��Ҫѭ��ִ�еĴ���
+        // 此处编写需要循环执行的代码
 
 
-        Adc.LeftHori  = adc_mean_filter_convert(ADC0_CH4_A4,10);  //���ҵ�вɼ�
+        Adc.LeftHori  = adc_mean_filter_convert(ADC0_CH4_A4,10);  //左右电感采集
         Adc.RightHori = adc_mean_filter_convert(ADC0_CH7_A7,10);
         Adc.LeftVert = adc_mean_filter_convert(ADC0_CH5_A5,10);  //181 147 38  76  179
         Adc.RightVert = adc_mean_filter_convert(ADC0_CH6_A6,10); //126 16  115 207 225
 
         get_Adc_MaxMin();
         get_Adc_Err();
-        //turn();            //ת��
+        //turn();            //转弯
         angle_num = test1();
 
         //if((Adc.LeftHori < 20) && (Adc.RightHori < 20)){speed = 20;}
 
-        pwm_set_duty(ATOM0_CH1_P33_9,angle_num);           //���
+        pwm_set_duty(ATOM0_CH1_P33_9,angle_num);           //舵机
 
-        pwm_set_duty(ATOM0_CH6_P02_6,1600);                //ת��
+        pwm_set_duty(ATOM0_CH6_P02_6,1600);                //转速
 
         ips_show();
 
 
-        // �˴���д��Ҫѭ��ִ�еĴ���
+        // 此处编写需要循环执行的代码
     }
 }
 
 #pragma section all restore
 
 
-// **************************** �������� ****************************
+// **************************** 代码区域 ****************************
